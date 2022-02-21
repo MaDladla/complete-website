777ef6fdbe683079b91387ca3c06fcb76c54d4dc
MaDladla <99400582+MaDladla@users.noreply.github.com>
2022-02-21 10:39:14 +0200

    Add files via upload

---
+++ node.js
@@ -0,0 +1,43 @@
+var header1, header2,header3, header4;
+var aboutMe, edBackround, tertiary, skill1, skill2, personal;
+var contacts,emails, phoneNum,address;
+var myName,bio,text1,text2;
+var cont,cont1,cont2,cont3,cont4;
+
+//--------------------------------AboutMe-------------------------------------------------------------------------------------
+header1= "About Me";
+document.getElementById('test').innerHTML=header1;
+aboutMe= "cynthia she is one person who never shy away from difficulties,dedication, passion and hard-work are some of the attributes that drives me and I always bring them in what ever I do. I'am a solid communicator and a fast learner. I am always valued with my ability to deal with complex situations by coming with unique solutios. I am a bubbly person and open minded, this makes it easier for me to work with a teams and I'm always ready to learn new things.";
+document.getElementById('test2').innerHTML=aboutMe;
+
+header2="Personal Details";
+document.getElementById('personalD').innerHTML=header2;
+personal="I'am cynthia letty dladla, gender female, I was born on the 11 january 1999, ID number: 9901111055083, race black, home language is Isizulu, i can also speak English, Xitsong and SiPedi, criminal record None, drivers license code 10";
+document.getElementById('personal').innerHTML=personal;
+
+header3= "Education Background"
+document.getElementById('education').innerHTML=header3;
+edBackround="completed matric 2016 at siyifunile secondary school";
+document.getElementById('edu').innerHTML= edBackround;
+tertiary="currently doing my final year in computer system Engineering at tshwane university of technology";
+document.getElementById('tertiary1').innerHTML=tertiary;
+
+
+
+header4="Skills";
+document.getElementById('head3').innerHTML=header4;
+skill1="-Ability to deal with complex situation by coming up with unique solutions and ideas";
+document.getElementById('skill').innerHTML= skill1;
+skill2="-keeping positive attitude even under pressure";
+document.getElementById('skills').innerHTML=skill2;
+
+//----------------------------footer--------------------------------------------------------------------
+contacts="Contact Details";
+document.getElementById('contact').innerHTML=contacts;
+emails="Email address: cynthiap16dladla@gmail.com";
+document.getElementById('email').innerHTML=emails;
+phoneNum="Phone number: 0729595846";
+document.getElementById('number').innerHTML=phoneNum;
+address=cont="contact details";
+address="155 troye street, sunnyside, pretoria, 0002"
+document.getElementById('address').innerHTML=address;
